75611d85d6d7576187ff3d2e7f00ef959714f08a
Iiro Krankka <iiro.krankka@codemate.com>
2018-03-28 23:48:00 +0300

    Removed the "try again" button for showtimes page when there's simply not showtimes to show for today.

--- lib/ui/error_view.dart
+++ lib/ui/error_view.dart
@@ -29,46 +29,50 @@ class InfoMessageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var content = <Widget>[
+      new CircleAvatar(
+        child: new Icon(
+          Icons.info_outline,
+          color: Colors.black54,
+          size: 52.0,
+        ),
+        backgroundColor: Colors.black12,
+        radius: 42.0,
+      ),
+      new Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: new Text(
+          title,
+          style: new TextStyle(fontSize: 24.0),
+        ),
+      ),
+      new Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: new Text(
+          description,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    ];
+
+    if (onActionButtonTapped != null) {
+      content.add(new Padding(
+        padding: const EdgeInsets.only(top: 12.0),
+        child: new FlatButton(
+          onPressed: onActionButtonTapped,
+          child: new Text(
+            'TRY AGAIN',
+            style: new TextStyle(color: theme.primaryColor),
+          ),
+        ),
+      ));
+    }
 
     return new Container(
       child: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new CircleAvatar(
-              child: new Icon(
-                Icons.info_outline,
-                color: Colors.black54,
-                size: 52.0,
-              ),
-              backgroundColor: Colors.black12,
-              radius: 42.0,
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: new Text(
-                title,
-                style: new TextStyle(fontSize: 24.0),
-              ),
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: new Text(
-                description,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 12.0),
-              child: new FlatButton(
-                onPressed: onActionButtonTapped,
-                child: new Text(
-                  'TRY AGAIN',
-                  style: new TextStyle(color: theme.primaryColor),
-                ),
-              ),
-            ),
-          ],
+          children: content,
         ),
       ),
     );
