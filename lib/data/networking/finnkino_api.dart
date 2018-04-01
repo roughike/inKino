836a7ac7ba54b810a55c9893db7e322837d675de
Iiro Krankka <iiro.krankka@codemate.com>
2018-04-01 21:32:24 +0300

    Cleanup networking code.

--- lib/data/networking/finnkino_api.dart
+++ lib/data/networking/finnkino_api.dart
@@ -1,51 +1,47 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
-import 'package:inkino/data/models/event.dart';
+import 'package:http/http.dart' as http;
 import 'package:inkino/data/models/theater.dart';
 import 'package:intl/intl.dart';
 
 class FinnkinoApi {
+  static final DateFormat ddMMyyyy = new DateFormat('dd.MM.yyyy');
+
   static final Uri kScheduleBaseUrl =
       new Uri.https('www.finnkino.fi', '/en/xml/Schedule');
   static final Uri kEventsBaseUrl =
       new Uri.https('www.finnkino.fi', '/en/xml/Events');
 
   Future<String> getSchedule(Theater theater, DateTime date) async {
-    var thing = date ?? new DateTime.now();
-    var dt = new DateFormat('dd.MM.yyyy').format(thing);
-
-    return _performGetRequest(
+    var dt = ddMMyyyy.format(date ?? new DateTime.now());
+    var response = await http.get(
       kScheduleBaseUrl.replace(queryParameters: {
         'area': theater.id,
         'dt': dt,
       }),
     );
+
+    return response.body;
   }
 
   Future<String> getNowInTheatersEvents(Theater theater) async {
-    return _performGetRequest(
+    var response = await http.get(
       kEventsBaseUrl.replace(queryParameters: {
         'area': theater.id,
         'listType': 'NowInTheatres',
       }),
     );
+
+    return response.body;
   }
 
   Future<String> getUpcomingEvents() async {
-    return _performGetRequest(
+    var response = await http.get(
       kEventsBaseUrl.replace(queryParameters: {
         'listType': 'ComingSoon',
       }),
     );
-  }
 
-  Future<String> _performGetRequest(Uri uri) async {
-    var httpClient = new HttpClient();
-    var request = await httpClient.getUrl(uri);
-    var response = await request.close();
-
-    return response.transform(UTF8.decoder).join();
+    return response.body;
   }
 }
