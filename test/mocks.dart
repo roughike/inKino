72c8df22620e0e5d6dba3252b246da4fc2b5048b
Iiro Krankka <iiro.krankka@codemate.com>
2018-04-01 21:01:45 +0300

    Removed the file cache for now.

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:inkino/data/file_cache.dart';
 import 'package:inkino/data/networking/finnkino_api.dart';
 import 'package:inkino/data/models/show.dart';
 import 'package:inkino/redux/app/app_state.dart';
@@ -14,5 +13,4 @@ class MockFinnkinoApi extends Mock implements FinnkinoApi {}
 class MockAssetBundle extends Mock implements AssetBundle {}
 
 class MockStore extends Mock implements Store<AppState> {}
-class MockCache extends Mock implements FileCache {}
 class MockShow extends Mock implements Show {}
