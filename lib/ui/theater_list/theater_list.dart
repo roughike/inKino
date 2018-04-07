f6eecc21337146e517243023148c3e224c277909
Iiro Krankka <iiro.krankka@codemate.com>
2018-04-07 12:59:21 +0300

    Some minor cleanup for theater list.

--- lib/ui/theater_list/theater_list.dart
+++ lib/ui/theater_list/theater_list.dart
@@ -18,6 +18,7 @@ class TheaterList extends StatelessWidget {
     var statusBarHeight = MediaQuery.of(context).padding.vertical;
 
     return new Transform(
+      // FIXME: A hack for drawing behind the status bar, find a proper solution.
       transform: new Matrix4.translationValues(0.0, -statusBarHeight, 0.0),
       child: new StoreConnector<AppState, TheaterListViewModel>(
         distinct: true,
@@ -56,11 +57,12 @@ class TheaterListContent extends StatelessWidget {
 
         var theater = viewModel.theaters[index - 1];
         var isSelected = viewModel.currentTheater.id == theater.id;
+        var backgroundColor = isSelected
+            ? const Color(0xFFEEEEEE)
+            : Theme.of(context).canvasColor;
 
         return new Material(
-          color: isSelected
-              ? const Color(0xFFEEEEEE)
-              : Theme.of(context).canvasColor,
+          color: backgroundColor,
           child: new ListTile(
             onTap: () {
               viewModel.changeCurrentTheater(theater);
