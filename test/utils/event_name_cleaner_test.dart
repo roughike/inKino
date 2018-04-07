516806ecbd14c4f459fb4c0363d4326755014a01
Iiro Krankka <iiro.krankka@codemate.com>
2018-04-07 19:44:31 +0300

    Added some more test cases for the event name cleaner & fixed one issue.

--- test/utils/event_name_cleaner_test.dart
+++ test/utils/event_name_cleaner_test.dart
@@ -18,6 +18,11 @@ void main() {
       expect(EventNameCleaner.cleanup('Avengers: Infinity War (3D dub)'), 'Avengers: Infinity War');
       expect(EventNameCleaner.cleanup('Avengers: Infinity War (3D orig)'), 'Avengers: Infinity War');
       expect(EventNameCleaner.cleanup('Avengers: Infinity War (3D spanish)'), 'Avengers: Infinity War');
+
+      // These should stay the same
+      expect(EventNameCleaner.cleanup('BPM (beats per minute)'), 'BPM (beats per minute)');
+      expect(EventNameCleaner.cleanup('That awesome spanish girl'), 'That awesome spanish girl');
+      expect(EventNameCleaner.cleanup('The 3D movie'), 'The 3D movie');
     });
   });
 }
