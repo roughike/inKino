4f2db7bfa2496a09f6499050d9c3400b2f9ab969
Iiro Krankka <iiro.krankka@gmail.com>
2019-01-19 11:10:16 +0100

    Place .travis.yml to repo root.

---
+++ web/test/sample_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+
+void main() {
+  /// TODO: Remove this and add proper tests.
+  test('trues are true and falses are false', () {
+    expect(true, isTrue);
+    expect(false, isFalse);
+  });
+}
