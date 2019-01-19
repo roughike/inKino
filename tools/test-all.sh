f09be6999acd78be6d953bbae965d0571f919f5b
Iiro Krankka <iiro.krankka@gmail.com>
2019-01-19 11:57:43 +0100

    Use full path when running flutter commands.

--- tools/test-all.sh
+++ tools/test-all.sh
@@ -8,7 +8,7 @@ echo "--- Running tests in core... ---"
 (cd core && pub run test)
 
 echo "--- Running tests in mobile... ---"
-(cd mobile && flutter test)
+(cd mobile && ./flutter/bin/flutter test)
 
 echo "--- Running tests in web... ---"
 (cd web && pub run build_runner test --fail-on-severe -- -p chrome)
