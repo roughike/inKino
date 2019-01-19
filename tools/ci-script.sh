a4437f475b5e653f171bd2b628124e74f474c38f
Iiro Krankka <iiro.krankka@gmail.com>
2019-01-19 11:50:24 +0100

    Get all packages before analyzing or testing anything.

--- tools/ci-script.sh
+++ tools/ci-script.sh
@@ -5,5 +5,6 @@ set -o pipefail
 set -o nounset
 
 ./tools/rename-tmdb-config-file.sh
+./tools/get-packages.sh
 ./tools/analyze-all.sh
 ./tools/test-all.sh
---
+++ tools/get-packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+(cd core && pub get)
+(cd web && pub get)
+(cd mobile && flutter packages get)
