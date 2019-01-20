1b1491fac1192e27a404445741ca5e103b9d6df7
Iiro Krankka <iiro.krankka@gmail.com>
2019-01-20 11:12:40 +0100

    Bump version codes.

---
+++ release-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+(cd mobile && flutter build apk && flutter build ios)
+
+(cd mobile/android && fastlane internal)
+(cd mobile/ios && fastlane beta)
